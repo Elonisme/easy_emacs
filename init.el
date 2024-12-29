9310832717e3314b0664d60817592fd13d098158
elonisme <elonisme@keemail.me>
2024-12-29 21:41:40 +0800

    update init.el

--- init.el
+++ init.el
@@ -23,18 +23,4 @@
   (require 'init-themes)
   )
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   '("04aa1c3ccaee1cc2b93b246c6fbcd597f7e6832a97aaeac7e5891e6863236f9f" "b11edd2e0f97a0a7d5e66a9b82091b44431401ac394478beb44389cf54e6db28" "76ddb2e196c6ba8f380c23d169cf2c8f561fd2013ad54b987c516d3cabc00216" "0a2168af143fb09b67e4ea2a7cef857e8a7dad0ba3726b500c6a579775129635" "6bdc4e5f585bb4a500ea38f563ecf126570b9ab3be0598bdf607034bb07a8875" "6fc9e40b4375d9d8d0d9521505849ab4d04220ed470db0b78b700230da0a86c1" default))
- '(package-selected-packages
-   '(color-theme-sanityinc-tomorrow tommyh-theme nordic-night-theme quickrun org-download visual-fill-column org-auto-tangle org-fragtog org-appear org-superstar markdown-preview-mode markdown-mode restart-emacs yasnippet-snippets yasnippet company-statistics company-box counsel ivy which-key neotree all-the-icons multiple-cursors)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+;; end init.el
